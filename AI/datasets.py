ef0beb968a3fc4e2f7d7e8857e86622be209b4ef
Linfer7892 <wnsehd06@g.skku.edu>
2025-06-02 23:17:45 +0900

    Update datasets.py

--- AI/datasets.py
+++ AI/datasets.py
@@ -1,9 +1,14 @@
 from torchvision import datasets, transforms
 
-def load_datasets(name, train=True) :
-    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(0.5, 0.5)])
+def load_dataset(name, train=True):
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
 
-    if name == "cifar10" :
-        return datasets.CIFAR10(root = "./data", train=train, download=True, transform=transform)
-    elif name == "cifar100" :
-        return datasets.CIFAR100(root = "./data", train=train, download=True, transform=transform)
+    if name.lower() == "cifar10":
+        return datasets.CIFAR10(root="./data", train=train, download=True, transform=transform)
+    elif name.lower() == "cifar100":
+        return datasets.CIFAR100(root="./data", train=train, download=True, transform=transform)
+    else:
+        raise ValueError(f"Dataset '{name}' is not supported.")
