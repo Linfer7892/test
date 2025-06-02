307f91722c50df3ecebb8e61defa85b121220e92
Linfer7892 <wnsehd06@g.skku.edu>
2025-06-02 23:19:45 +0900

    Update __init__.py

--- AI/models/__init__.py
+++ AI/models/__init__.py
@@ -1,14 +1,17 @@
-from .ResNet import ResNet34, ResNet50
+from .ResNet import ResNet34
 from .PreActResNet import PreActResNet
+from .densenet import DenseNet
+from .fractalnet import FractalNet
 
-def load_model(model_name, num_classes=10) :
-    if model_name == "resnet34" :
+def load_model(model_name, num_classes=10):
+    model_name = model_name.lower()
+    if model_name == "resnet34":
         return ResNet34(num_classes=num_classes)
-    elif model_name == "resnet50" :
-        return ResNet50(num_classes=num_classes)
-    elif model_name == "preactresnet" :
+    elif model_name == "preactresnet":
         return PreActResNet(num_classes=num_classes)
-    elif model_name == "densenet" :
-        pass
-    elif model_name == "fractalnet" :
-        pass
+    elif model_name == "densenet" or model_name == "desnetnet":
+        return DenseNet(num_classes=num_classes)
+    elif model_name == "fractalnet":
+        return FractalNet(num_classes=num_classes)
+    else:
+        raise ValueError(f"Model '{model_name}' is not supported.")
