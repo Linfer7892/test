618c4ac03c1d1296b100bfaca469e94fbf48c2fa
Linfer7892 <wnsehd06@g.skku.edu>
2025-06-25 20:18:27 +0900

    Update datasets.py

--- AI/datasets.py
+++ AI/datasets.py
@@ -1,10 +1,19 @@
 from torchvision import datasets, transforms
 
 def load_dataset(name, train=True):
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
+    # train/test에 따라 다른 transform 적용
+    if train:
+        transform = transforms.Compose([
+            transforms.RandomResizedCrop(32, scale=(0.8, 1.0)),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+    else:
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
 
     if name.lower() == "cifar10":
         return datasets.CIFAR10(root="./data", train=train, download=True, transform=transform)
