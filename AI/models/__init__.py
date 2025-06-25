8850180d1599aedf2cff48d1e517f6e552add199
Linfer7892 <wnsehd06@g.skku.edu>
2025-06-25 20:20:38 +0900

    Update __init__.py

--- AI/models/__init__.py
+++ AI/models/__init__.py
@@ -1,17 +1,22 @@
-from .ResNet import ResNet34
-from .PreActResNet import PreActResNet
-from .densenet import DenseNet
-from .fractalnet import FractalNet
+from .ResNet import resnet18, resnet34
+from .PreActResNet import preactresnet18
 
-def load_model(model_name, num_classes=10):
+def load_model(model_name, data_name):
+    # num_classes 설정
+    data_name = data_name.lower()
+    if data_name == "cifar100":
+        num_classes = 100
+    elif data_name == "cifar10":
+        num_classes = 10
+    else:
+        num_classes = 10
+    
     model_name = model_name.lower()
-    if model_name == "resnet34":
-        return ResNet34(num_classes=num_classes)
-    elif model_name == "preactresnet":
-        return PreActResNet(num_classes=num_classes)
-    elif model_name == "densenet" or model_name == "desnetnet":
-        return DenseNet(num_classes=num_classes)
-    elif model_name == "fractalnet":
-        return FractalNet(num_classes=num_classes)
+    if model_name == "resnet18":
+        return resnet18(num_classes=num_classes)
+    elif model_name == "resnet34":
+        return resnet34(num_classes=num_classes)
+    elif model_name == "preactresnet18":
+        return preactresnet18(num_classes=num_classes)
     else:
         raise ValueError(f"Model '{model_name}' is not supported.")
