4818cf2c703565b4e87392bed145c6fe3c714c9a
Linfer7892 <wnsehd06@g.skku.edu>
2025-06-25 20:20:22 +0900

    Update ResNet.py

--- AI/models/ResNet.py
+++ AI/models/ResNet.py
@@ -2,24 +2,24 @@ import torch
 import torch.nn as nn
 
 class ResidualBlock(nn.Module) :
-
+    constant = 1
     def __init__(self, in_channels, out_channels, stride=1) :
-        constant = 1
+        
         super().__init__()
         self.relu = nn.ReLU()
 
         self.residual_block = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1),
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(),
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(out_channels)
         )
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_channels != out_channels :
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, padding=0),
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, padding=0, bias=False),
                 nn.BatchNorm2d(out_channels)
             )
 
@@ -27,28 +27,28 @@ class ResidualBlock(nn.Module) :
         return self.relu(self.residual_block(x) + self.shortcut(x))
 
 class BottleneckBlock(nn.Module) :
-
+    constant = 4
     def __init__(self, in_channels, out_channels, stride=1) :
-        constant = 4
+        
         super().__init__()
         self.relu = nn.ReLU()
 
         self.bottleneck_block = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0),
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(),
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=stride, padding=1),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(),
-            nn.Conv2d(out_channels, constant*out_channels, kernel_size=1, stride=1, padding=0),
-            nn.BatchNorm2d(constant*out_channels),
+            nn.Conv2d(out_channels, self.constant*out_channels, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(self.constant*out_channels),
         )
 
         self.shortcut = nn.Sequential()
-        if stride != 1 or in_channels != constant*out_channels :
+        if stride != 1 or in_channels != self.constant*out_channels :
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, constant*out_channels, kernel_size=1, stride=stride, padding=0),
-                nn.BatchNorm2d(constant*out_channels)
+                nn.Conv2d(in_channels, self.constant*out_channels, kernel_size=1, stride=stride, padding=0, bias=False),
+                nn.BatchNorm2d(self.constant*out_channels)
             )
 
     def forward(self, x) :
@@ -65,13 +65,16 @@ class ResNet(nn.Module):
             nn.ReLU()
         )
 
-        self.layer1 = self._make_layer(block, 64, layers[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.conv2 = self._make_layer(block, 64, layers[0], stride=1)
+        self.conv3 = self._make_layer(block, 128, layers[1], stride=2)
+        self.conv4 = self._make_layer(block, 256, layers[2], stride=2)
+        self.conv5 = self._make_layer(block, 512, layers[3], stride=2)
 
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(512 * block.constant, num_classes)
+        self.classifier = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Flatten(),
+            nn.Linear(512 * block.constant, num_classes)
+        )
 
     def _make_layer(self, block, out_channels, num_blocks, stride):
         strides = [stride] + [1] * (num_blocks - 1)
@@ -79,18 +82,15 @@ class ResNet(nn.Module):
         for s in strides:
             layers.append(block(self.in_channels, out_channels, s))
             self.in_channels = out_channels * block.constant
-            
         return nn.Sequential(*layers)
 
     def forward(self, x):
         x = self.stem(x)
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-        x = self.avgpool(x)
-        x = torch.flatten(x, 1)
-        x = self.fc(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        x = self.classifier(x)
         return x
     
 def resnet18(num_classes=10):
